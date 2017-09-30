28137ce29ed1c551c7d52cc75fa013f052e12cbd
zhu2015210402076 <1293408468@qq.com>
2017-09-30 09:51:33 +0800

    the third time

--- HelloWorld/views.py
+++ HelloWorld/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 #coding:utf-8
-#from django.http import HttpResponse
+from django.http import HttpResponse
 def index(request):
     return render(request,"index.html",)
        #return HttpResponse(u"Hello World! 大家好！")
